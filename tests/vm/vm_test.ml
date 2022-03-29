b90e7816ba9d13df3c7f7cc2a0bd40be2f9e4f4d
Renato Alencar <renatoalencar.73@gmail.com>
2022-03-29 17:12:24 -0300

    lambda_vm: raise traceable exceptions on vm_test

--- tests/vm/vm_test.ml
+++ tests/vm/vm_test.ml
@@ -4,22 +4,24 @@ type error =
   | Compilation_error of compile_error
   | Execution_error   of execution_error
 
+exception Vm_test_error of error
+
 let failwith s = Format.kasprintf failwith s
 
 let compile_exn gas script =
   match compile gas script with
   | Ok value -> value
-  | Error error -> failwith "Compilation_error(%a)" pp_compile_error error
+  | Error error -> raise (Vm_test_error (Compilation_error error))
 
 let compile_value_exn gas value =
   match compile_value gas value with
   | Ok value -> value
-  | Error error -> failwith "Compilation_error(%a)" pp_compile_error error
+  | Error error -> raise (Vm_test_error (Compilation_error error))
 
 let execute_exn gas arg script =
   match execute gas ~arg script with
   | Ok value -> value
-  | Error error -> failwith "Execution_error(%a)" pp_execution_error error
+  | Error error -> raise (Vm_test_error (Execution_error error))
 
 let execute_ast_exn gas arg script =
   (* TODO: Use different gas to different stuff *)
